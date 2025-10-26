50268e13c952144e0a24ae3dc21aa6283bc0ff42
Daniel Chin <daniel.chin@nyu.edu>
2025-10-26 18:47:27 +0400

    get meta of liked

---
+++ get_meta_of_liked.fish
@@ -0,0 +1,27 @@
+yt-dlp 'https://www.youtube.com/playlist?list=LL' \
+    --skip-download \
+    --dump-json \
+    --write-info-json \
+    --output './Liked/%(id)s.%(ext)s' \
+    # --cookies ~/temp/cookies.txt \
+    --cookies ~/temp/cookies.firefox-private.txt \
+    # --cookies-from-browser firefox \
+    | jq -r '{
+        id: .id,
+        title: .title,
+        channel: .channel,
+        uploader: .uploader,
+        description: .description,
+        categories: .categories,
+        chapters: .chapters,
+        duration: .duration,
+        _filename: ._filename,
+        filename: .filename,
+        _type: ._type,
+        _version: ._version,
+        acodec: .acodec,
+        abr: .abr,
+        asr: .asr,
+        audio_channels: .audio_channels
+    }' \
+    > ./liked_meta.json
