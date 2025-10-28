46d19847d424709ce324192499c9350f1a79cd84
Daniel Chin <daniel.chin@nyu.edu>
2025-10-28 17:54:26 +0400

    [untested] classify music

---
+++ classify_music_or_video.py
@@ -0,0 +1,57 @@
+import json
+import random
+
+from gpt_arbiter_human_in_loop import UI, ArbiterDummy
+
+META = './liked/meta.json'
+PROMPT_AND_EXAMPLES_FILENAME = './liked/prompt_and_examples.json'
+RW_JSON_PATH = './liked/classifications.json'
+LAMBDA = 20
+MODEL = 'gpt-5-nano'
+
+def main():
+    with open(META, 'r', encoding='utf-8') as f:
+        meta = json.load(f)
+    d = { x['id']: x for x in meta }
+    all_ids = [*d.keys()]
+    random.shuffle(all_ids) # i.i.d. is important
+    def idToClassifiee(id_: str) -> str:
+        entry = d[id_]
+        title = entry['title']
+        channel = entry['channel']
+        description = entry['description']
+        categories = entry['categories']
+        chapters = entry['chapters']
+        duration = entry['duration']
+
+        categories = '; '.join(categories)
+        chapters = [x['title'] for x in chapters]
+        h = duration // 3600
+        m = duration // 60 - h * 60
+        s = duration - h * 3600 - m * 60
+        duration = f'{h} h {m} m {s} s'
+        duration = duration.removeprefix('0 h ')
+        duration = duration.removeprefix('0 m ')
+
+        return json.dumps(dict(
+            title=title            [:60],
+            channel=channel        [:60],
+            categories=categories  [:60],
+            description=description[:60],
+            chapters=chapters      [:60],
+            duration=duration      [:60],
+        ), indent=2, ensure_ascii=False)
+
+    ui = UI(
+        arbiter=ArbiterDummy(),
+        prompt_and_examples_filename='./prompt_and_examples.json',
+        all_ids=all_ids,
+        idToClassifiee=idToClassifiee,
+        rw_json_path=RW_JSON_PATH,
+        Lambda=LAMBDA,
+        model_name=MODEL,
+    )
+    ui.run()
+
+if __name__ == "__main__":
+    main()
